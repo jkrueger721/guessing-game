3abd9b55a5cce06e62108675841112c148f91b8f
jkrueger721 <johnathankrueger@me.com>
2023-04-18 14:28:13 -0400

    adde err handling to the parse

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess");
     loop {
-        let mut guess = String::new();
+        println!("Please input your guess");
 
+        let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("you guessed: {guess}");
 
         match guess.cmp(&secret_number) {
